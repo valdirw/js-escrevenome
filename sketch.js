296d6e6ab03601626b8ea0fec9c50afce80d3a71
valdirw <110181619+valdirw@users.noreply.github.com>
2024-08-02 07:30:02 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//-- declarando variavel global
+let palavra;
+function setup() {
+  createCanvas(400, 400);
+  let palavras = ["Caminhante", "caminho", "caminha"];
+  palavra = random(palavras);
+}
+function inicializaCores() {
+  background("white");
+  fill("black");
+  textSize(64);
+  textAlign(CENTER,CENTER);
+
+}
+function draw() {
+  inicializaCores();
+  let maximo = width;
+  let minimo = 0;
+  //mouseX, 0, width ==> 0, palavra.length
+  
+  let quantidade = map(mouseX, 0, width, 1, palavra.length);
+  //console.log(quantidade);
+  let parcial = palavra.substring(0, quantidade);
+  text(parcial,200,200);
+ 
+}
